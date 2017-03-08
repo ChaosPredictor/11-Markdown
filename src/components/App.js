d9522f249f29cef3827bb158934c221026c75e02
chaospredictor <kuzi81@gmail.com>
2017-03-09 00:19:16 +0200

    three component: title, main & footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ require('styles/App.css');
 
 import React from 'react';
 import Footer from './Footer';
+import Main from './Main';
+import Title from './Title';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
+				<Title />
+				<Main />
 				<Footer />
       </div>
     );
