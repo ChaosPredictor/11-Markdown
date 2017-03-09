1c73a04cc6439e7d3cb7c822426b913103dcd16f
chaospredictor <kuzi81@gmail.com>
2017-03-09 12:36:06 +0200

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,16 @@ require('normalize.css/normalize.css');
 require('styles/Footer.css');
 
 import React from 'react';
+import { Col } from 'react-bootstrap';
 
 class FooterComponent extends React.Component {
   render() {
     return (
-      <div className="footer">
-				test
-      </div>
-    );
+			<footer>
+				<Col md={6}>coded by <a class="text-danger" target="_blank" href="https://www.freecodecamp.com/chaospredictor">Chaos Predictor</a></Col>
+				<Col md={6} className="rightFooter">souces in <a class="text-danger" target="_blank" href="https://github.com/ChaosPredictor/11-Markdown">GitHub</a></Col>
+			</footer>
+		);
   }
 }
 
