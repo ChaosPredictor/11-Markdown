add80a7be7c9e8dea691bdb80adce6cc379bdf5c
chaospredictor <kuzi81@gmail.com>
2017-03-09 12:01:39 +0200

    version w/o warnings

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ require('styles/Main.css');
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-var marked = require('react-marked');
+//var marked = require('react-marked');
 import { MarkdownPreview } from 'react-marked-markdown';
 
 class MainComponent extends React.Component {
@@ -15,27 +15,22 @@ class MainComponent extends React.Component {
   }
 
   handleChange(e) {
-    this.setState({inputText: e.target.value, outputText: e.target.value});
-  }
-
-  getMarkdownText(text) {
-    var rawMarkup = marked(text || '');
-    return { __html: rawMarkup };
+    this.setState({inputText: e.target.value});
   }
 
   render() {
-    return (
-		<div> 
-			<Grid>
-				<Row className="show-grid">
-					<Col md={6}>
-						<textarea className="inputBox" value={this.state.inputText} onChange={this.handleChange} />
-					</Col>
-					<Col md={6}><MarkdownPreview value={ this.state.inputText }/></Col>
-				</Row>
-			</Grid>
-		</div>
-    );
+		return(
+			<div>
+				<Grid>
+					<Row className="show-grid">
+						<Col md={6}>
+							<textarea className="inputBox" value={this.state.inputText} onChange={this.handleChange} />
+						</Col>
+						<Col md={6}><MarkdownPreview value={ this.state.inputText }/></Col>
+					</Row>
+				</Grid>
+			</div>
+		);
   }
 }
 
